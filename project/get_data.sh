3e23291d2e27d32cb27830d442b47e0c314fe157
Gitlittlerubbish <1293555247@qq.com>
2020-01-25 19:48:45 +0000

    First batch of data attempt.

--- project/get_data.sh
+++ project/get_data.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 # University College of London
 # This is a script of SNS project's first step to get the <IP address, throughput> pair.
@@ -11,9 +11,9 @@ VERSION="0.0.2"
 rm -f logfile*
 
 cat chenxiao.icon
-echo "|        \033[33mFetching <IP, Throughput> Script V$VERSION\033[0m       |"
-echo "|           $ \033[34mChen Xiao \033[0m <uceeheh@ucl.ac.uk> $         |"
-echo "\n\nTHIS MAY TAKE A WHILE, PLEASE BE PATIENT WHILE THE SCRIPT IS RUNNING..."
+echo -e "|        \033[33mFetching <IP, Throughput> Script V$VERSION\033[0m       |"
+echo -e "|           $ \033[34mChen Xiao \033[0m <uceeheh@ucl.ac.uk> $         |"
+echo -e "\n\nTHIS MAY TAKE A WHILE, PLEASE BE PATIENT WHILE THE SCRIPT IS RUNNING..."
 
 input="./domains.txt"
 
@@ -27,10 +27,10 @@ do
 	for idx in {0..39}
 	do
 		printf "▓"
-		wget -4 --tries=1 --connect-timeout=3 --report-speed=bits --append-output=./logs/logfile"_$line"  -O /dev/null $line
+		wget -4 --tries=1 --timeout=3 --report-speed=bits --append-output=./logs/logfile"_$line"  -O /dev/null $line
 		if [ $? != 0 ]
 		then
-			echo "\n\033[31m"$line" connection failed, go to the next domain.\033[0m"
+			echo -e "\n\033[31m"$line" connection failed, go to the next domain.\033[0m"
 			echo "$line" >> err_domains.txt
 			errflag=1
 			break
